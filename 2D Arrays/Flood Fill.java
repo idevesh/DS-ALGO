3bbe6eb52d539b3aab131168194b1440694e2a48
Devesh Pratap Singh <deveshps107@gmail.com>
2021-09-19 22:18:31 +0530

    Create Flood Fill.java

---
+++ 2D Arrays/Flood Fill.java
@@ -0,0 +1,32 @@
+// Java Code to find out the flood filling using the DFS.
+// https://leetcode.com/problems/flood-fill/
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int i,j;
+        int prevColor = image[sr][sc];
+        if(prevColor != newColor)
+            color(image, sr, sc, newColor, prevColor);
+        return image;
+    }
+    
+    public void color(int[][] image, int sr, int sc, int newColor, int color) {
+        
+        try {
+            if(image[sr][sc] != color ||
+               sr < 0 || 
+               sr >= image.length || sc < 0 || 
+               sc >= image[0].length){
+                return;
+            }
+        } catch(Exception e){
+            return;
+        }
+        
+        image[sr][sc] = newColor;
+        color(image, sr-1, sc, newColor, color);
+        color(image, sr+1, sc, newColor, color);
+        color(image, sr, sc-1, newColor, color);
+        color(image, sr, sc+1, newColor, color);
+    }
+}
