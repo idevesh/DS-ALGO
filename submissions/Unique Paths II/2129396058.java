6a80b95d0548c8a248f4e9fbfd553d30c6bcd18b
Devesh Pratap Singh <deveshps107@gmail.com>
2026-09-05 21:57:03 +0530

    Add submission 2129396058

---
+++ submissions/Unique Paths II/2129396058.java
@@ -0,0 +1,38 @@
+# Title: Unique Paths II
+# Submission ID: 2129396058
+# Status: Accepted
+# Date: 3 September 2026 at 14:05:16 GMT+5:30
+
+import java.util.Arrays;
+class Solution {
+    int[][] memorization;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        memorization = new int[m][n];
+        return uniquePaths(0, 0, 0, obstacleGrid, m, n, memorization);
+    }
+
+    public int uniquePaths(int posx, int posy, int paths, int[][] obstacleGrid, int m, int n, int[][] memorization) {
+        if(posx > m - 1 || posy > n - 1) {
+            return 0;
+        }
+
+        if(obstacleGrid[posx][posy] == 1) {
+            return 0;
+        }
+
+        if(posx == m - 1 && posy == n - 1) {
+            return ++paths;
+        }
+
+        if(memorization[posx][posy] == 0) {
+            paths += uniquePaths(posx + 1, posy, paths, obstacleGrid, m, n, memorization) + uniquePaths(posx, posy + 1, paths, obstacleGrid, m, n, memorization);  
+            memorization[posx][posy] = paths;
+        } else {
+            return paths + memorization[posx][posy];
+        }
+
+        return paths;
+    }
+}
