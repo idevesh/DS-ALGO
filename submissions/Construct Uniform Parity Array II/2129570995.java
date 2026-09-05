6b53e7fe3759f78b0fb853df569ab936065f88d2
Devesh Pratap Singh <deveshps107@gmail.com>
2026-09-05 21:57:00 +0530

    Add submission 2129570995

---
+++ submissions/Construct Uniform Parity Array II/2129570995.java
@@ -0,0 +1,31 @@
+# Title: Construct Uniform Parity Array II
+# Submission ID: 2129570995
+# Status: Accepted
+# Date: 3 September 2026 at 17:28:43 GMT+5:30
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        if(nums1.length == 0 || nums1.length == 1) return true;
+
+        int oddCount = 0, evenCount = 0;
+        for(int num: nums1) {
+            if(num % 2 != 0) {
+                oddCount++;
+            } else {
+                evenCount++;
+            }
+        }
+
+        if(evenCount == 0 || oddCount == 0) return true;
+        
+        int min = Arrays.stream(nums1)
+                        .min()
+                        .getAsInt();
+                        
+        if(min % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
