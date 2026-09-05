812c25e89846f570fb0b78547bca8d1cfd9bb5e6
Devesh Pratap Singh <deveshps107@gmail.com>
2026-09-05 21:57:16 +0530

    Add submission 2121767087

---
+++ submissions/Jump Game II/2121767087.java
@@ -0,0 +1,31 @@
+# Title: Jump Game II
+# Submission ID: 2121767087
+# Status: Accepted
+# Date: 27 August 2026 at 15:33:45 GMT+5:30
+
+class Solution {
+    public int jump(int[] nums) {
+        int[] memory = new int[nums.length];
+        Arrays.fill(memory, -1);
+        return fetchJumps(nums, 0, memory);
+    }
+
+    private int fetchJumps(int[] nums, int i, int[] memory) {
+        if(memory[i] != -1) {
+            return memory[i];
+        }
+        if (i == nums.length - 1) {
+            return 0;
+        }
+        if (nums[i] == 0) {
+            return 1000000;
+        }
+        int finalJumps = 1000000;
+        int end = Math.min(nums.length, 1 + i + nums[i]);
+        for (int j = i + 1; j < end; j++) {
+            finalJumps = Math.min(finalJumps, 1 + fetchJumps(nums, j, memory));
+        }
+        memory[i] = finalJumps;
+        return finalJumps;
+    }
+} 
