cbf95e8d95e552c4f258be3dffc5023ed057e4ec
Devesh Pratap Singh <deveshps107@gmail.com>
2026-09-05 21:56:53 +0530

    Add submission 2131774407

---
+++ submissions/Smallest Stable Index II/2131774407.java
@@ -0,0 +1,37 @@
+# Title: Smallest Stable Index II
+# Submission ID: 2131774407
+# Status: Wrong Answer
+# Date: 5 September 2026 at 19:24:19 GMT+5:30
+
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        if(nums.length == 1) return nums[0];
+        int[] suffixArr = new int[nums.length];
+        buildSuffixArr(nums, suffixArr, 0);
+        System.out.print(Arrays.toString(suffixArr));
+        return firstStableIndex(nums, suffixArr, 0, k, Integer.MIN_VALUE);
+    }
+
+    private void buildSuffixArr(int[] nums, int[] suffixArr, int index) {
+        if(index == nums.length - 1) {
+            suffixArr[index] = nums[index];
+            return;
+        }
+
+        buildSuffixArr(nums, suffixArr, index+1);
+        suffixArr[index] = Math.min(suffixArr[index + 1], nums[index]);
+    }
+
+    private int firstStableIndex(int[] nums, int[] suffixArr, int index, int k, int max) {
+        if(index == nums.length) {
+            return -1;
+        }
+        max = Math.max(nums[index], max);
+
+        if(max - suffixArr[index] <= k) {
+            return index;
+        }
+
+        return firstStableIndex(nums, suffixArr, index + 1, k, max);
+    }
+}
