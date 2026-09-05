b5a8d7911226cb4744f8633a9a81c00cc7ccfe5f
Devesh Pratap Singh <deveshps107@gmail.com>
2026-09-05 21:56:57 +0530

    Add submission 2130639463

---
+++ submissions/Smallest Stable Index I/2130639463.java
@@ -0,0 +1,28 @@
+# Title: Smallest Stable Index I
+# Submission ID: 2130639463
+# Status: Accepted
+# Date: 4 September 2026 at 17:03:59 GMT+5:30
+
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+        for (int splitIndex = 0; splitIndex < nums.length; splitIndex++) {
+            int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+            // find maximum
+            for (int i = 0; i <= splitIndex; i++) {
+                max = Math.max(nums[i], max);
+            }
+
+            // min value
+            for (int i = splitIndex; i < nums.length; i++) {
+                min = Math.min(nums[i], min);
+            }
+
+            if (max - min <= k) {
+                return splitIndex;
+            }
+        }
+
+        return -1;
+    }
+}
